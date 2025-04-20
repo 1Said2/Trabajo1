86d89609a56d919a0c458d045b4362a3ca2d76b1
Said Cotacachi <oscotacachiu@utn.edu.ec>
2025-04-20 11:38:15 -0500

    Tomar tiempo ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,15 +1,16 @@
-import random
+import random, time
 
-# Generar dos arreglos A y B con números aleatorios positivos
-numelementos = 10  
-A = [random.randint(1, 100) for _ in range(numelementos)]
-B = [random.randint(1, 100) for _ in range(numelementos)]
+def main(cantidad_elementos=0):
+    arreglo_A = [random.randint(1, 100) for _ in range(cantidad_elementos)]
+    arreglo_B = [random.randint(1, 100) for _ in range(cantidad_elementos)]
 
-elementos_A_y_en_B = []
-for elemento_A in A:
-    if elemento_A in B and elemento_A not in elementos_A_y_en_B:
-        elementos_A_y_en_B.append(elemento_A)
-
-print("Arreglo A:", A)
-print("Arreglo B:", B)
-print("Elementos de A que están en B:", elementos_A_y_en_B)
+    elementos_A_y_en_B = []
+    inicio = time.time()
+    for elemento_A in arreglo_A:
+        if elemento_A in arreglo_B and elemento_A not in elementos_A_y_en_B:
+            elementos_A_y_en_B.append(elemento_A)
+    fin = time.time()
+    print("Arreglo arreglo_A:", arreglo_A)
+    print("Arreglo arreglo_B:", arreglo_B)
+    print("Elementos de arreglo_A que están en arreglo_B:", elementos_A_y_en_B)
+    return fin - inicio
