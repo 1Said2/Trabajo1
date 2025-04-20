202eb5c9c8d64de903f4a0dbe5984a5bb0c2bff9
Demon5855 <dmontesdeocatrujillo@gmail.com>
2025-04-19 21:00:33 -0500

    Ejericio 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,30 +1,33 @@
-def print_int_matrix(matrix):
-    for row in matrix:
-        print("\t".join(map(str, row)))
+import math
+
+
+def imprimir_matriz_int(matriz):
+    for fila in matriz:
+        print("\t".join(map(str, fila)))
     print()
 
 
-def main():
-    order = int(input("Ingrese el orden de la matriz: "))
+def main(tamanio = 0):
+    orden = int(math.sqrt(tamanio))
 
-    matrix = [[0] * order for _ in range(order)]
+    matriz = [[0] * orden for _ in range(orden)]
 
-    max_limit = (order * order + order) // 2
-    counter = 1
+    limite_maximo = (orden * orden + orden) // 2
+    contador = 1
 
-    for row in range(0, order, 2):
-        for column in range(row + 1):
-            matrix[row][column] = counter
-            counter += 1
+    for fila in range(0, orden, 2):
+        for columna in range(fila + 1):
+            matriz[fila][columna] = contador
+            contador += 1
 
-        row += 1
+        fila += 1
 
-        if counter < max_limit:
-            for column in range(row, -1, -1):
-                matrix[row][column] = counter
-                counter += 1
+        if contador < limite_maximo:
+            for columna in range(fila, -1, -1):
+                matriz[fila][columna] = contador
+                contador += 1
 
-    print_int_matrix(matrix)
+    imprimir_matriz_int(matriz)
 
 
 if __name__ == "__main__":
